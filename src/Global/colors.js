6035a03da4e67264663e7f6d8b793725350606a8
Hans Aroni <hans.aroni@infocorpgroup.com>
2024-02-09 19:52:05 -0300

    first commit

---
+++ src/Global/colors.js
@@ -0,0 +1,11 @@
+
+export const colors = {
+    lightGray:"#EDF1D6",
+    green1:"#9DC08B",
+    green2:"#609966",
+    green3:"#40513B",
+    blueviolet: "#8A2BE2",
+    cyan: "#00FFFF",
+    darkmagenta: "#8B008B",
+    deeppink: "#FF1493"
+}
---
+++ src/firebase/db.js
@@ -0,0 +1,3 @@
+export const base_url = "https://coderdev-3d040-default-rtdb.firebaseio.com/"
+export const auth_base_url = "https://identitytoolkit.googleapis.com/v1/"
+export const api_key = "AIzaSyDH2mFEXf9yBtxgefMPaTC3Ed_v5YHk90I"
